01b4bd9c96840ef8f8feb185bb1513938d33be11
weian404 <weian404@gmail.com>
2023-06-07 00:12:00 +0800

    feat(web): add web module

---
+++ narwhal-web/src/main/java/org/narwhal/springboot/web/filter/MDCFilter.java
@@ -0,0 +1,14 @@
+package org.narwhal.springboot.web.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class MDCFilter implements Filter {
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+  }
+
+
+}
